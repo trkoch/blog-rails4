5a81d2e813c41e9ce99f4c22a60d3731a6dafc38
Tristan Koch <tristan@tknetwork.de>
2014-06-11 15:10:33 +0200

    Add page resource
    Commands:
    * rails g resource page title body:text
    * rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611104730) do
+ActiveRecord::Schema.define(version: 20140611130655) do
 
   create_table "comments", force: true do |t|
     t.string  "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20140611104730) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "pages", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
