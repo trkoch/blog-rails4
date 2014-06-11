7c992c95635dadf0587189f21e759aca62330789
Tristan Koch <tristan@tknetwork.de>
2014-06-11 15:23:59 +0200

    Move posts to blog namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   namespace :blog do
     resources :comments
+    resources :posts
   end
 
   resources :pages
-
-  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
