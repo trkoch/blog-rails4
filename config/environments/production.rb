55aa612ded05cf390d9598dc871462ee7329e341
Tristan Koch <tristan@tknetwork.de>
2014-06-17 12:19:04 +0200

    Configure action mailer
    Read smtp_user and smtp_password from environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Action mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.secrets.smtp_user,
+    password:             Rails.application.secrets.smtp_password,
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {
+    from: 'no-reply@example.com'
+  }
+
 end
