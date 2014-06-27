ddc76ac960feb491e34899f40e49621985830055
Tristan Koch <tristan@tknetwork.de>
2014-06-27 11:54:27 +0200

    Fake error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  def self.all
+    some_method
+    super
+  end
+
+  def self.some_method
+    fail if some_str.present?
+  end
+
+  def self.some_str
+    "Some str"
+  end
 end
