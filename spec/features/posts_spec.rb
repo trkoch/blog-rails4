69d812311237c845e5fe9cc3c3bd4993f4f2764a
Tristan Koch <tristan@tknetwork.de>
2014-07-09 13:05:45 +0200

    Start implementing feature by adding failing test

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 describe "posts" do
+  after :each do
+    Post.delete_all
+  end
+
   it "shows listing" do
     Post.create!(title: "Some title", body: "Some body")
     Post.create!(title: "Another title", body: "Another body")
@@ -11,5 +15,13 @@ describe "posts" do
       expect(page).to have_css("tbody td", text: "Some title")
       expect(page).to have_css("tbody td", text: "Some body")
     end
+  end
+
+  it "shows most recent first" do
+    Post.create!(title: "Old", created_at: Date.new(2010))
+    Post.create!(title: "Most Recent")
+    visit "/blog/posts"
+    posts = page.all("table.posts tbody td:first-child")
+    expect(posts.first).to have_content("Most Recent")
   end
 end
