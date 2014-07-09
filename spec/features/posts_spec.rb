bcfd5b3707f247364656f4947fa3c9b076ff875b
Tristan Koch <tristan@tknetwork.de>
2014-07-09 11:04:10 +0200

    Expect existence of posts in listing

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -2,7 +2,12 @@ require "rails_helper"
 
 describe "posts" do
   it "shows listing" do
+    Post.create!(title: "Some title", body: "Some body")
+    Post.create!(title: "Another title", body: "Another body")
     visit "/blog/posts"
     expect(page).to have_content("Listing posts")
+    expect(page).to have_css("tbody tr", count: 2)
+    expect(page).to have_css("tbody td", text: "Some title")
+    expect(page).to have_css("tbody td", text: "Some body")
   end
 end
