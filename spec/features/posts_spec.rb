7f128332b55540e169f0f828475437ee798d4d61
Tristan Koch <tristan@tknetwork.de>
2014-07-10 13:17:16 +0200

    Improve organization of tests

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -5,23 +5,25 @@ describe "posts" do
     Post.delete_all
   end
 
-  it "shows listing" do
-    Post.create!(title: "Some title", body: "Some body")
-    Post.create!(title: "Another title", body: "Another body")
-    visit "/blog/posts"
-    expect(page).to have_content("Listing posts")
-    within "table.posts" do
-      expect(page).to have_css("tbody tr", count: 2)
-      expect(page).to have_css("tbody td", text: "Some title")
-      expect(page).to have_css("tbody td", text: "Some body")
+  describe "index" do
+    it "shows listing of posts" do
+      Post.create!(title: "Some title", body: "Some body")
+      Post.create!(title: "Another title", body: "Another body")
+      visit "/blog/posts"
+      expect(page).to have_content("Listing posts")
+      within "table.posts" do
+        expect(page).to have_css("tbody tr", count: 2)
+        expect(page).to have_css("tbody td", text: "Some title")
+        expect(page).to have_css("tbody td", text: "Some body")
+      end
     end
-  end
 
-  it "shows most recent first" do
-    Post.create!(title: "Old", created_at: Date.new(2010))
-    Post.create!(title: "Most Recent")
-    visit "/blog/posts"
-    posts = page.all("table.posts tbody td:first-child")
-    expect(posts.first).to have_content("Most Recent")
+    it "shows most recent post first" do
+      Post.create!(title: "Old", created_at: Date.new(2010))
+      Post.create!(title: "Most Recent")
+      visit "/blog/posts"
+      posts = page.all("table.posts tbody td:first-child")
+      expect(posts.first).to have_content("Most Recent")
+    end
   end
 end
