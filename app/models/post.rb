08cac0e39c2fc5d7f5da765a42e6be3dcf330755
Tristan Koch <tristan@tknetwork.de>
2014-07-10 14:59:45 +0200

    Refactor (step 2)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ActiveRecord::Base
   default_scope { order("created_at DESC") }
 
-  def excerpt
-    count = 100
+  def excerpt(count=100)
     return body if body.blank?
     return body if body.length <= count
     body[0...count] + "…"
