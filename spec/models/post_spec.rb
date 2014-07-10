ee84f5cf24ea70ce3531180d8028fe8a8a8969c9
Tristan Koch <tristan@tknetwork.de>
2014-07-10 14:26:16 +0200

    Expect not to truncate
    Add another expectation to better express our intent.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,11 @@ describe Post do
   let(:post) { Post.new }
 
   describe "excerpt" do
+    it "does not truncate when less or equal 100 characters" do
+      post.body = "Some body"
+      expect(post.excerpt).to eq(post.body)
+    end
+
     it "truncates body to first 100 characters" do
       post.body = "X" * 100 + "Z"
       expect(post.excerpt.length).to eq(100)
